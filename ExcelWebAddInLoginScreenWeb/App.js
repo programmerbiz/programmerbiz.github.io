68f05bcef0e191926d67fd106511ec927b9b78bf
guisselle <GuisselleM@ashn.com>
2016-10-21 14:02:21 -0400

    Initial commit

---
+++ ExcelWebAddInLoginScreenWeb/App.js
@@ -0,0 +1,95 @@
+﻿// Common app functionality
+
+var app = (function () {
+    'use strict';
+
+    var app = {};
+
+    //Change this value for every Add-In
+    app.addinName = "OAuthToken"
+
+    // Function called when the page has opened outside of the Add-In
+    app.getToken = function (tokenParams) {
+
+        var url = tokenParams.authServer +
+                    "response_type=" + encodeURI(tokenParams.responseType) + "&" +
+                    "client_id=" + encodeURI(tokenParams.clientId) + "&" +
+                    "resource=" + encodeURI(tokenParams.resource) + "&" +
+                    "redirect_uri=" + encodeURI(tokenParams.replyUrl);
+
+        // var winObj = window.open(url);
+        // winObj.focus();
+        location.href = url;
+    };
+
+    app.returnToken = function () {
+        var urlParameterExtraction = new (function () {
+            function splitQueryString(queryStringFormattedString) {
+                var split = queryStringFormattedString.split('&');
+
+                // If there are no parameters in URL, do nothing.
+                if (split == "") {
+                    return {};
+                }
+
+                var results = {};
+
+                // If there are parameters in URL, extract key/value pairs.
+                for (var i = 0; i < split.length; ++i) {
+                    var p = split[i].split('=', 2);
+                    if (p.length == 1)
+                        results[p[0]] = "";
+                    else
+                        results[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+                }
+                return results;
+            }
+
+            // Split the query string (after removing preceding '#').
+            this.queryStringParameters = splitQueryString(window.location.hash.substr(1));
+        })();
+
+        var token = urlParameterExtraction.queryStringParameters['access_token'];
+
+        if (token) {
+            app.setCookie(token, 3600);
+            app.tokenCallback(token)
+        } else {
+            document.write("Shame")
+        }
+        /*if (window.opener){
+            window.opener.app.tokenCallback(token);
+            window.close()
+        } else {
+            document.write("There appears to have been an error, please close the window and check with your administrator")
+        }*/
+
+    }
+
+    app.tokenCallback = function (token) {
+        //this would then continue to do what you really need in the Add-In
+        document.getElementById('tokenHere').innerHTML = token
+    }
+
+    app.setCookie = function (value, duration) {
+        var now = new Date();
+        var time = now.getTime();
+        var expireTime = time + 1000 * duration;
+        now.setTime(expireTime);
+        console.log("---setting cookie--");
+        document.cookie = app.addinName + "=" + value + ';expires=' + now.toGMTString() + ';path=/';
+        console.log(document.cookie);
+    }
+
+    app.getCookie = function (name) {
+        var pattern = RegExp(name + "=.[^;]*")
+        var matched = document.cookie.match(pattern)
+        if (matched) {
+            var cookie = matched[0].split('=')
+            return cookie[1]
+        }
+        return false
+    }
+
+    return app;
+})();
---
+++ ExcelWebAddInLoginScreenWeb/Home.js
@@ -0,0 +1,72 @@
+﻿/// <reference path="/Scripts/FabricUI/MessageBanner.js" />
+/// <reference path="App.js" />
+// global app
+
+(function () {
+    "use strict";
+
+    var messageBanner;
+
+    // The initialize function must be run each time a new page is loaded.
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            // Initialize the FabricUI notification mechanism and hide it
+            var element = document.querySelector('.ms-MessageBanner');
+            messageBanner = new fabric.MessageBanner(element);
+            messageBanner.hideBanner();
+            
+            // If not using Excel 2016, use fallback logic.
+            //if (!Office.context.requirements.isSetSupported('ExcelApi', '1.1')) {
+            //    $('#loginbuttontext').text("Login to CRM");
+            //    return;
+            //}
+
+ 
+            //Event Handlers
+            $('#loginbuttontext').text("Login to CRM");
+            $('#loginbutton').click(loginToCRM);
+        });
+    }
+    
+    function loginToCRM() {
+        //check to see if there is an OAuth Token cached in the cookie
+        //app.addinName will need to be different for every Add-In
+        var token = app.getCookie(app.addinName)
+        if (!token) {
+            var tokenParams = {};
+            tokenParams.authServer = 'https://login.windows.net/common/oauth2/authorize?';
+            tokenParams.responseType = 'token';
+            tokenParams.replyUrl = location.href.split("?")[0];
+
+            //THESE tokenParams need to be changed for your application
+            //tokenParams.clientId = 'Your-app-clientId-goes-here';
+            tokenParams.clientId = 'b3beda96-6b4e-4200-857f-cf75c5425054';
+            tokenParams.resource = "https://programmerbiz.github.io/ExcelWebAddInLoginScreenWeb/Home.html";
+            //tokenParams.resource = "https://yoursite.sharepoint.com";
+
+            app.getToken(tokenParams);
+        } else {
+            //we have a token therefore carry on
+            app.tokenCallback(token)
+        }
+    };
+
+
+    // Helper function for treating errors
+    function errorHandler(error) {
+        // Always be sure to catch any accumulated errors that bubble up from the Excel.run execution
+        showNotification("Error", error);
+        console.log("Error: " + error);
+        if (error instanceof OfficeExtension.Error) {
+            console.log("Debug info: " + JSON.stringify(error.debugInfo));
+        }
+    }
+
+    // Helper function for displaying notifications
+    function showNotification(header, content) {
+        $("#notificationHeader").text(header);
+        $("#notificationBody").text(content);
+        messageBanner.showBanner();
+        messageBanner.toggleExpansion();
+    }
+})();
